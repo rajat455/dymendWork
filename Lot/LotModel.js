75725afd63a9f75c5ebe80f05834c760c70a450a
PRATIKJADAV777 <jadavpratik347@gmail.com>
2024-01-11 15:16:43 +0530

    new update

--- Lot/LotModel.js
+++ Lot/LotModel.js
@@ -1,4 +1,5 @@
 const { default: mongoose } = require("mongoose");
+const { toching } = require("../Constents");
 
 class LotModel {
     constructor() {
@@ -6,15 +7,15 @@ class LotModel {
             lotNumber: { type: String, required: true },
             kapan: { type: mongoose.Types.ObjectId, required: true, ref: "tbl_kapans" },
             manager: { type: mongoose.Types.ObjectId, default: null, ref: "tbl_users" },
-            weight:{type:Number, required:true},
+            weight: { type: Number, required: true },
             totalPcs: { type: Number, required: true },
-            availabelPcs:{type:Number, required:true},
-            tochingPendingPcs:{type:Number,required:true, default:0},
-            tochingCompletedPcs:{type:Number, required:true, default:0},
-            fourpPendingPcs:{type:Number, required:true, default:0},
-            fourpCompletedPcs:{type:Number, required:true, default:0},
+            availabelPcs: { type: Number, required: true },
+            tochingPendingPcs: { type: Number, required: true, default: 0 },
+            tochingCompletedPcs: { type: Number, required: true, default: 0 },
+            fourpPendingPcs: { type: Number, required: true, default: 0 },
+            fourpCompletedPcs: { type: Number, required: true, default: 0 },
         }, {
-            timestamps:true,
+            timestamps: true,
             indexes: [
                 {
                     fields: ['kapan', 'lotNumber'],
@@ -25,9 +26,16 @@ class LotModel {
         this.model = mongoose.model("tbl_lots", this.schema)
     }
 
-    insertLot(data){
-        return this.model.create({...data})
+    insertLot(data) {
+        return this.model.create({ ...data })
     }
+
+    // Update Lot
+    updatePcs(purpose, lot, Pcs) {
+        return this.model.updateOne({ _id: lot }, { $inc: purpose == toching ? { availabelPcs: -Pcs, tochingPendingPcs: Pcs, } : { tochingCompletedPcs: -Pcs, fourpPendingPcs: Pcs } })
+    }
+
+
 }
 const lotModel = new LotModel()
 module.exports = lotModel
--- Work/WorkController.js
+++ Work/WorkController.js
@@ -1,9 +1,41 @@
+const { addAbortListener } = require("events")
 const { MISSING_DEPENDENCY, SOMTHING_WENT_WRONG, SUCCESS, INTERNAL_SERVER_ERROR, toching, fourP } = require("../Constents")
 const lotModel = require("../Lot/LotModel")
 const workModel = require("./WorkModel")
+const { CLIENT_RENEG_LIMIT } = require("tls")
+const { log } = require("console")
 
 class WorkConroller {
-   
+
+    async addWork(req, res) {
+        try {
+            const { lot, employee, purpose, totalPcs, completedPcs } = req.body
+            if (!lot || !employee || !purpose || !totalPcs) return res.status(400).send({ message: MISSING_DEPENDENCY })
+            const result = await workModel.insertLot(req.body)
+            if (!result) return res.status(400).send({ message: SOMTHING_WENT_WRONG })
+            await lotModel.updatePcs(purpose, lot, totalPcs)
+            return res.status(200).send({ message: SUCCESS })
+        } catch (error) {
+            console.log(error);
+            return res.status(500).send({ message: INTERNAL_SERVER_ERROR })
+        }
+    }
+
+    async listWork(req, res) {
+        try {
+            const result = await workModel.list()
+            if (!result) return res.status(400).send({ message: SOMTHING_WENT_WRONG })
+            return res.status(400).send({ message: SUCCESS })
+        } catch (error) {
+
+            return res.status(500).send({ message: INTERNAL_SERVER_ERROR })
+
+        }
+    }
+
+    async updateWork(req, res) {
+
+    }
 }
 
 const wrokConroller = new WorkConroller()
