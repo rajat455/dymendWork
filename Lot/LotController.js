da45902f0dbc0de0fc33c59ff031403cb31357f3
vinitpatel135 <patelvinit135@gmail.com>
2024-01-11 13:32:54 +0530

    up to date

--- Lot/LotController.js
+++ Lot/LotController.js
@@ -3,9 +3,28 @@ const lotModel = require("./LotModel")
 
 class LotController {
     
+    async addLot(req,res){
+        try {
+            const {lotNumber, kapan, weight, totalPcs, availabelPcs} = req.body
+            if (!lotNumber || !weight || !totalPcs || !kapan || !availabelPcs) return res.status(400).send({ message: MISSING_DEPENDENCY})
+            const result = await lotModel.insertLot({ ...req.body })
+            if (!result) return res.status(500).send({ message: SOMTHING_WENT_WRONG })
+            return res.status(200).send({message:SUCCESS})
+        } catch (error) {
+            if(error.code === 11000) return res.status(400).send({message:ALLREADY_EXIST})
+            return res.status(500).send({message:INTERNAL_SERVER_ERROR})
+        }
+    }
 
-
-    
+    async listLot(req,res){
+        try {
+            const result = await lotModel.model.find()
+            if(!result) return res.status(500).send({message:SOMTHING_WENT_WRONG})
+            return res.status(200).send({message:SUCCESS, data:result})
+        } catch (error) {
+            return res.status(500).send({message:INTERNAL_SERVER_ERROR})
+        }
+    }
 }
 
 const lotController = new LotController()
--- Lot/LotRouter.js
+++ Lot/LotRouter.js
@@ -1,6 +1,9 @@
 const express = require("express")
+const lotController = require("./LotController")
 
 const lotRouter = express.Router()
 
+lotRouter.post("/add", lotController.addLot)
+lotRouter.get("/", lotController.listLot)
 
 module.exports = lotRouter
